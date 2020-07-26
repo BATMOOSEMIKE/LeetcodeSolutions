4ffb8e6a0079c6f63560f1f6fb1cd06079ffbacf
Michael He <michael.he2001@gmail.com>
2020-07-25 17:56:25 -0700

    Finished number of students doing work at a given time

---
+++ numberOfStudentsDoingWorkAtAGivenTime.java
@@ -0,0 +1,12 @@
+//Runtime O(N)
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for(int i = 0; i < startTime.length; i++) {
+            if(queryTime <= endTime[i] && queryTime >= startTime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
