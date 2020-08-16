6725a514fbed192baebce30d3bfe11a4ea6e5cc3
Michael He <michael.he2001@gmail.com>
2020-08-16 13:15:51 -0700

    Finished average salary excluding minimum and maximum

---
+++ averageSalaryExcludingMininumAndMaximum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double average(int[] salary) {
+        int max = salary[0];
+        int min = salary[0];
+        int sum = 0;
+
+        for(int i = 0; i < salary.length; i++){
+            if(min > salary[i]) {
+                min = salary[i];
+            }
+            if(max < salary[i]) {
+                max = salary[i];
+            }
+            sum += salary[i];
+        }
+
+        return (((double) sum) - max - min)/(salary.length-2);
+    }
+}
